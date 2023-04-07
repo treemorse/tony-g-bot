434839ec2c364973334eb58a2ac6b76eefeffbc4
Mira <mirazondberg@Miras-iMac.local>
2023-04-08 01:10:40 +0300

    Final changes

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def telegram_bot(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        if update.message.text == "/phrase":
+        if update.message.text == "/phrase" or "/phrase@TonyGuogaBot":
             with open('sources/phrases.txt', 'r') as f:
                 num_lines = sum(1 for line in f)
             rand_line_num = random.randint(1, num_lines)
             rand_line = linecache.getline('sources/phrases.txt', rand_line_num).strip()
             bot.sendMessage(chat_id=chat_id, text=rand_line)
-        if update.message.text == "/labaz":
+        if update.message.text == "/labaz" or "/labaz@TonyGuogaBot":
             bot.sendMessage(chat_id=chat_id, text=f"Поздравляю, @{update.message.chat.username}, Вы настоящий лабазер!")
     return "okay"
