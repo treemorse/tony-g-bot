75e0feb4e8b7d33f5fb304c1bbbc494614f7d955
Mira <mirazondberg@192.168.1.10>
2023-04-07 22:48:44 +0300

    Recreated files, deleted .ignore files

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ def telegram_bot(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        # Reply with the same message
         bot.sendMessage(chat_id=chat_id, text=update.message.text)
     return "okay"
